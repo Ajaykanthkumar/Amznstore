b6ec0d6a4e74a484a49474c30ddc243ca3d6c61b
Ajaykanthkumar <151132427+Ajaykanthkumar@users.noreply.github.com>
2025-03-30 13:19:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Script loaded.");
+function filterProducts() {
+  const input = document.getElementById("search-input").value.toLowerCase();
+  const products = document.querySelectorAll(".product");
+
+  products.forEach((product) => {
+    const name = product.getAttribute("data-name").toLowerCase();
+    if (name.includes(input)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+}
